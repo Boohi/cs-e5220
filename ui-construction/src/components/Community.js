76315ef2b88f507d4c6d831ae10937e251cb3596
Aleksi Siitari <aleksi@Aleksis-MacBook-Pro.local>
2019-12-13 15:03:28 +0200

    community and preferences pages

--- ui-construction/src/components/Community.js
+++ ui-construction/src/components/Community.js
@@ -3,9 +3,51 @@ import React, { Component } from 'react'
 export default class Community extends Component {
     render() {
         return (
-            <div>
-                Community
+            <div class="container">
+
+            <div class="row">
+
+            	<div class="info-card margin-card card col-lg-4">
+            		<img class="img-fluid" src="./consumption_graph.png"></img>
+            	</div>
+            	<div class="info-card margin-card col-lg-4">
+            		<img class="img-fluid" src="./community_production_graph.png"></img>
+            	</div>
+            	<div class="margin-card">
+            		<div class="info-card blue-infobox">
+            			<i class="fas white fa-home"></i>
+            			<h4 class="white">35/50</h4>
+            			<p class="white">Active Households</p>
+            		</div>
+            		<div class="info-card blue-infobox">
+            			<i class="fas white fa-tags"></i>
+            			<h4 class="white">12,5 €/kW</h4>
+            			<p class="white">Average Price</p>
+            		</div>
+            	</div>
             </div>
+
+            <div class="row">
+            	<div class="info-card margin-card card col-lg-4">
+            		<span><h4 class="inline">210,0 €</h4><p class="inline small-text"> +15% </p><i class="inline fas green-text fa-arrow-circle-up"></i></span>
+            		<p>Monthly profit (By selling electricity)</p>
+            	</div>
+            	<div class="info-card margin-card card col-lg-4">
+            		<span><h4 class="inline">4,5 MTCO2</h4><p class="inline small-text"> -4,5% </p><i class="inline fas red-text fa-arrow-circle-down"></i></span>
+            		<p>Monthly carbon footprint reduction</p>
+            	</div>
+            </div>
+
+            <div class="row">
+            	<div class="info-card margin-card card col-lg-4">
+            		<span><h4 class="inline">92,0 €</h4><p class="inline small-text"> +2% </p><i class="inline fas green-text fa-arrow-circle-up"></i></span>
+            		<p>Monthly savings (By buying from community)</p>
+            	</div>
+            </div>
+     
+            </div>
+  
+
         )
     }
 }
--- ui-construction/src/components/MyPreferences.js
+++ ui-construction/src/components/MyPreferences.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react'
 export default class MyPreferences extends Component {
     render() {
         return (
-            <div>
-                MyPreferences
+            <div class="container">
+            <span class="glyphicon glyphicon-pencil"></span>
+	            <div class="row">
+	                <div class="info-card col-lg-8 col-md-12">
+	                	<img class="img-fluid" src="/production_chart.png" alt="Production chart"></img>
+	                </div>
+	                <div class="col-lg-1 col-sm-1 col-md-1"></div>
+	                <div class=" col-lg-2 row">
+	                	<div class="info-card">
+	                		<h6>Selling Price (Own Supply)</h6>
+	                		<span><h5><strong>14,50 €/kW</strong></h5> <button class="edit-button"><i class="blue fas fa-edit"></i></button></span>
+	                	</div>
+	                	<div class="col-sm-1"></div>
+	                	<div class="info-card">
+	                		<h6>Buying Price (Community Supply)</h6>
+	                		<span><h5><strong>12,87 €/kW</strong></h5><button class="edit-button"><i class="blue fas fa-edit"></i></button></span>
+	                	</div>
+	                </div>
+	            </div>
             </div>
         )
     }
