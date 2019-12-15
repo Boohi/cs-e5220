8131777f9a155a0c6983fca490925671ddcddbff
Otto Mäenpää <otto.maenpaa@gmail.com>
2019-12-15 23:27:06 +0200

    added active header

--- ui-construction/src/components/Navbar.js
+++ ui-construction/src/components/Navbar.js
@@ -3,36 +3,47 @@ import { Link, withRouter } from "react-router-dom";
 import "./styles.css";
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {active: "community"}
+    }
+
+
+    changeActive(active) {
+        this.setState({active});
+    }
+
 	render() {
 		return (
 			<nav className="navbar navbar-expand-lg landing-navbar py-2">
-				<Link className="navbar-brand" to="/">
+				<Link className="navbar-brand" to="/" onClick={() => this.changeActive("community")}>
 					EcoEle
 				</Link>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">
+                        <li className={"nav-item"} onClick={() => this.changeActive("community")}>
+                            <Link className={"nav-link " + (this.state.active === "community" ? "activeLink" : "")} to="/">
                                 Community
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/preferences">
+                        <li className={"nav-item "} onClick={() => this.changeActive("preferences")}>
+                            <Link className={"nav-link " + (this.state.active === "preferences" ? "activeLink" : "")} to="/preferences">
                                 Preferences
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/mydevices">
+                        <li className={"nav-item "} onClick={() => this.changeActive("mydevices")}>
+                            <Link className={"nav-link " + (this.state.active === "mydevices" ? "activeLink" : "")} to="/mydevices">
                                 My Devices
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/provider">
+                        <li className={"nav-item "} onClick={() => this.changeActive("provider")}>
+                            <Link className={"nav-link " + (this.state.active === "provider" ? "activeLink" : "")} to="/provider">
                                 Provider
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/settings">
+                        <li className={"nav-item "} onClick={() => this.changeActive("settings")}>
+                            <Link className={"nav-link " + (this.state.active === "settings" ? "activeLink" : "")} to="/settings">
                                 <img
                                     src="/face-pic.png"
                                     alt={"Robert Warren"}
